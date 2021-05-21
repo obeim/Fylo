3b7ce419328b84582eff3525bf2e5f59cb0d5ddb
obei <obei.m2017@gmail.com>
2021-05-21 17:58:41 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+app.listen(port, () => console.log(`running in port ${port}`));
